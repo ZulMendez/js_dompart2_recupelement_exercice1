089cf7a5de697d1d5a51b4119a6b54a845dbf02b
zulma <zumendez7@gmail.com>
2021-01-27 09:53:53 +0100

    exo terminé

---
+++ main.js
@@ -0,0 +1,13 @@
+// 1
+let text1 = document.getElementsByClassName('tartine');
+console.log(text1[0].innerText);
+
+// 2
+let text2 = document.getElementById('toast')
+console.log(text2.innerText);
+
+// 3
+let elem = document.querySelectorAll('p')[3];
+console.log(elem);
+
+// console.log(text1[1]);
